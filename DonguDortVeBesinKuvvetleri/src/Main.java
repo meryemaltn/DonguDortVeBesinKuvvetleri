f9801e0159e4b051088b2b252d0b96cddd929be3
Meryem Altunkaya <meryemaltunkaya@hotmail.com>
2021-06-25 15:59:34 +0300

    First commit

--- DonguDortVeBesinKuvvetleri/src/Main.java
+++ DonguDortVeBesinKuvvetleri/src/Main.java
@@ -3,16 +3,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int n,i=0;
+		int n,i=1;
 		Scanner inp= new Scanner (System.in);
 		System.out.println("Sayýyý Giriniz: ");
 		n=inp.nextInt();
 		
-		while (i<=n) {
-
-			if (i%4==0 && i%5==0) {
-				System.out.println(i);
-			}i++;
+		for (i=1 ; i<=n ; i*=4) {
+			System.out.println(i);
+		}
+		
+		for (i=5 ; i<=n ; i*=5) {
+			System.out.println(i);
+		}
 		}
 	}
-}
